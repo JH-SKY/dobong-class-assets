1558a97cc1dafc04e58cd41b09fe5dd8835df271
jonghyeok0512 <fpdltldwkd@gmail.com>
2026-02-03 14:13:01 +0900

    깃허브규칙수정

---
+++ fastapi/fastapi_basic/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+from mysite.post_api import router as post_router
+from mysite2.post_api import router as pydantic_router
+
+app = FastAPI()
+app.include_router(post_router)
+app.include_router(pydantic_router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello")
+def hello():
+    return "Hello World!"
+
+
+@app.get("/hi")
+def hi():
+    return ["hello", "world", "!"]
+
+
+@app.get("/odd")
+def odd():
+    result = []
+    for i in range(100):
+        if i % 2 == 0:
+            result.append(i)
+
+    return result
